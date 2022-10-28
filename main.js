7fd54f192cc169ef35f70f42043009f7ee9268a2
grav09 <grafimaulanas@gmail.com>
2022-10-28 12:42:21 +0700

    second summit

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const db = require('./connection');
+// database lokal
+// const db = require('./connection');
 const { response } = require('express');
 const { url } = require('inspector');
 const app = express()
@@ -56,32 +57,40 @@ app.get('/home', (req, res) => {
     res.end
 })
 
-//auth
-app.post('/auth', (req, res) => {
-    let username = req.body.username
-    let password = req.body.password
-    if(username && password){
-        db.query('SELECT * FROM user WHERE username = ? AND password = ?',
-        [username, password],(error, results, field) => {
-            if(error)throw error;
-            if(results.length > 0){
-                //auth
-                req.session.loggedin = true;
-                req.session.username = username;
-                req.session.jenis = results[0].jenis;
-                //redirect
-                res.redirect('./home')
-            }else{
-                res.send("incorrect username & password")
-            }
-            res.end()
-        })
-    }else{
-        res.send('please enter username & password')
-        res.end
-    }        
-    // res.redirect('./home')
-});
+//auth database lokal
+// app.post('/auth', (req, res) => {
+//     let username = req.body.username
+//     let password = req.body.password
+//     if(username && password){
+//         db.query('SELECT * FROM user WHERE username = ? AND password = ?',
+//         [username, password],(error, results, field) => {
+//             if(error)throw error;
+//             if(results.length > 0){
+//                 //auth
+//                 req.session.loggedin = true;
+//                 req.session.username = username;
+//                 req.session.jenis = results[0].jenis;
+//                 //redirect
+//                 res.redirect('./home')
+//             }else{
+//                 res.send("incorrect username & password")
+//             }
+//             res.end()
+//         })
+//     }else{
+//         res.send('please enter username & password')
+//         res.end
+//     }        
+// });
+
+//auth 
+
+// fetch('https://dummyjson.com/users')
+// .then(res => res.json())
+// .then(console.log);
+            
+
+
 app.post('/logout', (req,res) => {
     // Hapus sesi user dari broser
     req.session.destroy((err) => {
@@ -94,30 +103,41 @@ app.post('/logout', (req,res) => {
         res.redirect('/');
     });
 })
-// db.query('select * from user',(error, result) =>{
-//     console.log(result)
-// })
-// console.log('so')
+
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+let get = (url) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(console.log())
+}
+
+console.log(get('https://dummyjson.com/products'))
+
+
+// GET data
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(data => {
+    // const dat = JSON.parse(JSON.stringify(data))
+    app.get('/p',(req,res)=>{
+        // dengan database lokal
+        // db.query("SELECT * FROM user", (err, result)=>{
+        //     const dbo = JSON.parse(JSON.stringify(result))
+        //     // console.log(dbo)
+        // })
+        res.render("index",{datas : data.products, title:"nama"})
+    })
+    console.log()
+});
+// let i = 0;
+// while(i<data.length){
+//     console.log(i++);
+// 
+
 app.use(express.static("public"));
 const port = 8020;
 app.listen(port,() => {
     console.log(`Server berjalan di localhost : ${port}`)
-});
-
-app.set("view engine", "ejs")
-app.set("views", "views")
-const get = fetch('https://dummyjson.com/products')
-db.query("SELECT * FROM user", (err, result)=>{
-    const data = JSON.parse(JSON.stringify(result))
-    console.log(data);
-})
-// fetch data
-fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then(data => {
-    const dat = JSON.parse(JSON.stringify(data))
-    app.get('/p',(req,res)=>{
-        res.render("index",{datas : data, title:"nama"})
-    })
-    console.log(dat)
 });
